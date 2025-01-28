65912a93e5f341d40540d8fda8c32a393e2bf81c
LuisSinastre <luisgustavosinastre13@gmail.com>
2025-01-28 00:09:14 -0300

    feat: adjustments on project files and login page

---
+++ fixflow-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+// src/App.tsx
+import React from 'react';
+import RoutesApp from './routes';
+
+function App () {
+    return (
+        <div>
+            <RoutesApp />
+        </div>
+    );
+}
+
+export default App;
---
+++ fixflow-frontend/src/routes.tsx
@@ -0,0 +1,17 @@
+// src/routes.tsx
+import React from 'react';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
+import LoginPage from './pages/login/login';
+
+
+function RoutesApp() {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+    );
+}
+
+export default RoutesApp;
