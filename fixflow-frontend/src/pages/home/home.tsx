e63a48562a61da5e58c6f9a9f8561561fa6f396c
LuisSinastre <luisgustavosinastre13@gmail.com>
2025-02-08 15:05:29 -0300

    feat: creating contexts to authenticate user

--- fixflow-frontend/src/pages/home/home.tsx
+++ fixflow-frontend/src/pages/home/home.tsx
@@ -1,22 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../contexts/authenticate";
 
 
 const HomePage: React.FC = () => {
-    
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
+  const authContext = useContext(AuthContext);
 
-    useEffect(() => {
-        const token = localStorage.getItem("token");
+  if (!authContext) {
+    return null;
+  }
 
-        if (token) {
-            setIsAuthenticated(true); // Se o token existir, o usuário está autenticado
-          }
-        }, []);
+  const { isAuthenticated, logout } = authContext;
 
     return (
         <div>
         {isAuthenticated ? (
+          <>
           <h1>Bem-vindo, você está autenticado!</h1>
+          <button onClick={logout}>Sair</button>
+          </>
         ) : (
           <h1>Por favor, faça login para acessar</h1>
         )}
